71c26b6f1510b284440a89fbafbd19e45e17d797
e_rafealplsleavemealone <6455robloxtv@gmail.com>
2025-02-02 22:43:01 +0000

    Update espeak.js

--- build/www/js/lib/espeak/espeak.js
+++ build/www/js/lib/espeak/espeak.js
@@ -1,156 +1,77 @@
-function Espeak(worker_path, ready_cb) {
-  this.worker = new Worker(worker_path);
+function Speak(workerPath, readyCb) {
+  this.worker = new Worker(workerPath);
   this.ready = false;
-  this.worker.onmessage = function(e) {
+  this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  this.worker.onmessage = function (e) {
     if (e.data !== 'ready') return;
     this.worker.onmessage = null;
     this.worker.addEventListener('message', this);
     this.ready = true;
-    if (ready_cb) {
-      ready_cb();
+    if (readyCb) {
+      readyCb();
     }
   }.bind(this);
 }
 
-Espeak.prototype.handleEvent = function (evt) {
+Speak.prototype.handleEvent = function (evt) {
   var callback = evt.data.callback;
   if (callback && this[callback]) {
     this[callback].apply(this, evt.data.result);
-    if (evt.data.done)
-      delete this[callback];
+    if (evt.data.done) delete this[callback];
     return;
   }
 };
 
-function _createAsyncMethod(method) {
-  return function() {
-    var lastArg = arguments[arguments.length - 1];
-    var message = { method: method, args: Array.prototype.slice.call(arguments, 0) };
-    if (typeof lastArg == 'function') {
-      var callback = '_' + method + '_' + Math.random().toString().substring(2) +'_cb';
-      this[callback] = lastArg;
-      message.args.pop();
-      message.callback = callback;
-    }
-    this.worker.postMessage(message);
+Speak.prototype.play = function (text, args, onended, onstart) {
+  var source = this.audioContext.createBufferSource();
+  var worker = this.worker;
+
+  source.stopOld = source.stop;
+  source.stop = function () {
+    this.stopOld();
+    if (this.endTimeout) clearTimeout(this.endTimeout);
   };
-}
 
-for (var i = 0; i < 8; i++) {
-  var method = [
-    'listVoices',
-    'get_rate',
-    'get_pitch',
-    'set_rate',
-    'set_pitch',
-    'setVoice',
-    'synth'
-  ][i];
-  Espeak.prototype[method] = _createAsyncMethod(method);
-}
-
-/* An audio node that can have audio chunks pushed to it */
-
-function PushAudioNode(context, start_callback, end_callback, stop_callback) {
-  this.start_callback = start_callback;
-  this.end_callback = end_callback;
-  this.stop_callback = stop_callback;
-  this.run_stop_callback = true;
-  this.samples_queue = [];
-  this.context = context;
-  this.scriptNode = context.createScriptProcessor(
-    isDesktop || false ? 0 : 8192, 1, 1
-  );
-  this.connected = false;
-  this.sinks = [];
-  this.startTime = 0;
-  this.closed = false;
-  this.initialized = false;
-}
-
-PushAudioNode.prototype.push = function(chunk) {
-  if (this.closed)
-    throw "can't push more chunks after node was closed";
-  this.samples_queue.push(chunk);
-  if (!this.connected) {
-    if (!this.sinks.length)
-      throw "No destination set for PushAudioNode";
-    this._do_connect();
-  }
-};
-
-PushAudioNode.prototype.close = function() {
-  this.closed = true;
-};
-
-PushAudioNode.prototype.connect = function(dest) {
-  this.sinks.push(dest);
-  if (this.samples_queue.length) {
-    this._do_connect();
-  }
-};
-
-PushAudioNode.prototype._do_connect = function() {
-  if (this.connected) return;
-  this.connected = true;
-  for (var dest in this.sinks) { // ???????
-    this.scriptNode.connect(this.sinks[dest]);
-  }
-  this.scriptNode.onaudioprocess = this.handleEvent.bind(this);
-};
-
-PushAudioNode.prototype.disconnect = function() {
-  this.scriptNode.onaudioprocess = null;
-  this.scriptNode.disconnect();
-  if (this.run_stop_callback)
-    this.stop_callback();
-  this.connected = false;
-};
-
-PushAudioNode.prototype.handleEvent = function(evt) {
-  var bufferSize = evt.target.bufferSize;
-
-  if (!this.startTime) {
-    this.startTime = evt.playbackTime;
-    if (this.start_callback) {
-      this.start_callback();
-    }
-    this.close_finished = false;
-    this.initialized = true;
-  }
-
-  var offset = 0;
-
-  while (this.samples_queue.length && offset < bufferSize) {
-    var chunk = this.samples_queue[0];
-    var to_copy = chunk.subarray(0, bufferSize - offset);
-    if (evt.outputBuffer.copyToChannel) {
-      evt.outputBuffer.copyToChannel(to_copy, 0, offset);
+  function startSource(source) {
+    if (source.start) {
+      source.start(0);
     } else {
-      evt.outputBuffer.getChannelData(0).set(to_copy, offset);
+      source.noteOn(0);
     }
-    offset += to_copy.length;
-    chunk = chunk.subarray(to_copy.length);
-    if (chunk.length)
-      this.samples_queue[0] = chunk;
-    else
-      this.samples_queue.shift();
+    if (onstart) onstart(source);
   }
 
+  function playSound(streamBuffer) {
+    source.connect(this.audioContext.destination);
+    this.audioContext.decodeAudioData(streamBuffer, function (audioData) {
+      source.buffer = audioData;
+      var duration = audioData.duration;
+      var delay = duration ? Math.ceil(duration * 1000) : 1000;
+      source.endTimeout = setTimeout(onended, delay);
+      startSource(source);
+    });
+  }
 
-  if (!this.samples_queue.length && this.closed) {
-    var end_delay = Math.max(0, (((bufferSize - 2048) / 2048) * 0.120) * 1000);
-    setTimeout((function() {
-      if (!this.close_finished) {
-        if (this.end_callback) {
-          this.end_callback(evt.playbackTime - this.startTime);
-        }
-        this.run_stop_callback = false;
-      }
-      this.disconnect();
-      this.close_finished = true;
-     }).bind(this), 
-     end_delay
-    );
+  function handleWav(wav) {
+    var buffer = new ArrayBuffer(wav.length);
+    new Uint8Array(buffer).set(wav);
+    playSound(buffer);
+  }
+
+  if (args && args.noWorker) {
+    var wav = generateSpeech(text, args);
+    playSound(wav);
+  } else {
+    worker.onmessage = function (event) {
+      handleWav(event.data);
+    };
+    worker.postMessage({ text: text, args: args });
   }
 };
+
+Speak.prototype.noConflict = function () {
+  window.speak = this._speak;
+  return this;
+};
+
+window.Speak = Speak;
