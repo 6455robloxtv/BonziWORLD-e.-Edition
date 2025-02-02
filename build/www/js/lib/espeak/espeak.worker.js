a2ea9213e243d3bbf469a2d3a47698be393fc11a
e_rafealplsleavemealone <6455robloxtv@gmail.com>
2025-02-02 22:54:15 +0000

    Update espeak.worker.js

--- build/www/js/lib/espeak/espeak.worker.js
+++ build/www/js/lib/espeak/espeak.worker.js
@@ -1,5 +1,9 @@
-importScripts('/js/lib/speakjs/tts.js');
-
-onmessage = function(event) {
-  postMessage(generateSpeech(event.data.text, event.data.args));
-};
+if('function' === typeof importScripts) {
+  importScripts('speakGenerator.js');
+  var self = this;
+  self.addEventListener('message', (event) => {
+    try {
+      postMessage(generateSpeech(event.data.text, event.data.args));
+    } catch {};
+  });
+}
